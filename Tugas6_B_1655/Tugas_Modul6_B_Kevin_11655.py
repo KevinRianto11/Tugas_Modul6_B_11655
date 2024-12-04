fd6718bc08547bec3eb2e2858094638b463e1df0
Kevin Stevano Rianto <220711655@students.uajy.ac.id>
2024-12-04 15:56:56 +0700

    Update Tugas_Modul6_B_Kevin_11655.py

--- Tugas6_B_1655/Tugas_Modul6_B_Kevin_11655.py
+++ Tugas6_B_1655/Tugas_Modul6_B_Kevin_11655.py
@@ -4,7 +4,7 @@ import numpy as np
 from tensorflow.keras.models import load_model
 from PIL import Image
 
-model = load_model(r'C:\Users\kevst\OneDrive\Documents\Kuliah\Sem 5\Pembelajaran Mesin dan Pembelajaran Mendalam\Tugas6_X_YYYYY\Tugas6_B_11655\model_mobilenet.h5')
+model = load_model(r'model_mobilenet.h5')
 class_names = ['Matang', 'Mentah']
 
 def classify_image(image_path):
